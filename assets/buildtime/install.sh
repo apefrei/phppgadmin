73d23424299e108e973bb8bf48b0518f7ed9c638
Lauro Frei <laurofrei@outlook.com>
2023-05-29 10:32:42 +0200

    added script file for config overwrite

--- assets/buildtime/install.sh
+++ assets/buildtime/install.sh
@@ -8,6 +8,7 @@ curl -SLO https://github.com/phppgadmin/phppgadmin/archive/master.zip
 unzip master.zip
 mv ./phppgadmin-master/* ${RAPTUS_WEBROOT_DIR}
 mv ${RAPTUS_WEBROOT_DIR}/conf/config.inc.php-dist ${RAPTUS_WEBROOT_DIR}/conf/config.inc.php
+${RAPTUS_ETC_DIR}/buildtime/phppgadmin-conf.sh
 chown -R nginx:nginx ${RAPTUS_WEBROOT_DIR}
 
 rm -rf master.zip phppgadmin-master
